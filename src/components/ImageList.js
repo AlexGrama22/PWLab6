ec1e7738af1065ec3e5c2eb1db93d8aec714b78d
Apple <Apple@Mihais-MacBook-Pro.local>
2024-04-24 18:11:55 +0300

    buttons added + responsive

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,23 @@
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageShow from "./ImageShow";
+import { Grid } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function ImageListComp({ images }) {
+  const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.only('md'))
+  const xs = useMediaQuery(theme.breakpoints.only('xs'))
+  const sm = useMediaQuery(theme.breakpoints.only('sm'))
+  let cols = 6
+  if(md) cols = 4
+  else if(xs) cols = 1
+  else if(sm) cols = 2
+
+
   return (
-    <ImageList sx={{ width: '100%', height: '100%' }} cols={6}>
+    <ImageList sx={{ width: '100%', height: '100%' }} cols={cols} rowHeight={164}>
       {images.map((image) => (
         <ImageListItem key={image.id}>
           <ImageShow image={image} />
--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -1,13 +1,26 @@
 import { Box } from '@mui/system';
+import { Button } from '@mui/material';
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { createSvgIcon } from '@mui/material/utils';
+import Grid from '@mui/material/Grid';
 
 
 function ImageShow (image) {
     return <div>
-      {/* <img src={image.urls.regular} alt={image.alt_description} /> */}
-      <Box height={209} display="flex" justifyContent="center" alignItems="center" overflow={'hidden'} >
-        {/* <img width={'100%'} 
-        src={image.image.urls.regular} alt={image.image.alt_description} /> */}
-                      <Box component="img" src={image.image.urls.regular} alt={image.image.alt_description} sx={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      <Box height={209} display="flex" justifyContent="center" alignItems="center" overflow={'hidden'} >        
+          <Box component="img" src={image.image.urls.regular} alt={image.image.alt_description} sx={{ width: '100%', height: '100%', objectFit: 'cover' }}/>
+          <Grid container spacing={2} width={'60%'} sx={{position: 'absolute', bottom: '0', right:'0'}}>
+            <Grid item xs={6}>
+              <Button variant="contained" color="primary" href={image.image.links.download} target="_blank" sx={{ m: 1}}><DownloadForOfflineIcon/></Button>    
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant="contained" color="primary" href={image.image.links.download} target="_blank" sx={{ m: 1}}><FavoriteIcon/></Button>    
+            </Grid>
+   
+          </Grid>
+          
+
 
       </Box>
     </div>
